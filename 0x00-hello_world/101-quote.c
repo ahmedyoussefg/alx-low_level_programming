69f83442376af85476af92022c5c80c05d9550c7
ahmedyoussefg <a.goerany@gmail.com>
2023-03-16 08:32:47 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * This is the entry point to the program.
+  *
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
